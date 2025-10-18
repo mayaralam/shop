1417fc8b6c44fe3459eeb0fb029a7e620269de58
mayaralam <mayaralam17@icloud.com>
2025-10-18 04:31:00 +0300

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -14,22 +14,17 @@ export default function App() {
       <BrowserRouter>
         <Toaster />
         <Routes>
-          <Route path="/registration" element={<Registration />}></Route>
-          {/* <Route
-            path="/login"
-            element={token ? <Navigate to={""} /> : <LoginPage />}
-          ></Route> */}
+          <Route path="registration" element={<Registration />}></Route>
           <Route path="/" element={<Navigate to="/login" />} />
-          {/* <Route path="/home" element= {<HomePage />}></Route> */}
           <Route
-            path="/home"
+            path="home"
             element={token ? <HomePage /> : <Navigate to="/login" />}
           />
-          <Route path="/login" element={<LoginPage />}></Route>
-          <Route path="/shop" element={<ShopPage />}></Route>
-          <Route path="/cart" element={<CartPage />}></Route>
-          <Route path="/about" element={<AboutPage />}></Route>
-          <Route path="/contact" element={<ContactPage />}></Route>
+          <Route path="login" element={<LoginPage />}></Route>
+          <Route path="shop" element={<ShopPage />}></Route>
+          <Route path="cart" element={<CartPage />}></Route>
+          <Route path="about" element={<AboutPage />}></Route>
+          <Route path="contact" element={<ContactPage />}></Route>
           <Route path="*" element={<h1>404 not found</h1>}></Route>
         </Routes>
       </BrowserRouter>
